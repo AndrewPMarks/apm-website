02826d5e62fe8817dbcb277386e5bce37de16a46
InnersecApp <andrewpmarks@outlook.com>
2020-10-28 16:44:48 -0600

    Added project images

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -14,9 +14,10 @@ export const projects: ProjectType[] = [
 			'Lorem ipsum dolor sit amet, duis excepteur enim irure nisi nostrud velit occaecat tempor duis culpa exercitation sint elit minim consectetur culpa officia et pariatur',
 	},
 	{
-		name: 'Sudoku',
-		link: '/sudoku',
+		name: 'AndrewMarks.io',
+		link: '/',
+		image: '/images/projects/andrewmarks.io.png',
 		description:
-			'Reprehenderit laboris tempor do ex eu in ad labore incididunt Lorem cillum incididunt incididunt exercitation consectetur ad nisi exercitation deserunt id magna excepteur proident ullamco',
+			'Lorem ipsum dolor sit amet, duis excepteur enim irure nisi nostrud velit occaecat tempor duis culpa exercitation sint elit minim consectetur culpa officia et pariatur',
 	},
 ];
