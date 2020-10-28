6cb794cbfa702a6a01c661fab658be46249d0547
InnersecApp <andrewpmarks@outlook.com>
2020-10-27 18:47:47 -0600

    Added project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const Project = () => {
-	return <div>Project!</div>;
+type ProjectProps = {
+	key: number;
+	project: {
+		name: string;
+		link: string;
+	};
+};
+
+export const Project = (props: ProjectProps) => {
+	let { name, link } = props.project;
+	return (
+		<article>
+			<Link to={link}>{name}</Link>
+		</article>
+	);
 };
--- src/pages/PageProjects.tsx
+++ src/pages/PageProjects.tsx
@@ -4,14 +4,18 @@ import { Project } from '../components/Project';
 export const PageProjects = () => {
 	const projects = [
 		{
-			name: 'Project Name',
+			name: 'Project 1',
+			link: '/projects',
+		},
+		{
+			name: 'Project 2',
 			link: '/projects',
 		},
 	];
 	return (
 		<section className="projects">
 			{projects.map((project, index) => (
-				<Project key={index} />
+				<Project key={index} project={project} />
 			))}
 		</section>
 	);
