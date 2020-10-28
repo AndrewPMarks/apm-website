3b5d43a5c848e09d37890f90954d6b80bebb34e0
InnersecApp <andrewpmarks@outlook.com>
2020-10-27 22:05:48 -0600

    Added pride flag 🏳️‍🌈

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import PrideFlag from '../flag.png';
 
 export const Footer = () => {
 	return (
-		<div className="footer">
-			&copy; Andrew Marks {new Date().getFullYear()}
+		<div className='footer'>
+			&copy; Andrew Marks {new Date().getFullYear()}{' '}
+			<img src={PrideFlag} alt='Pride Flag' className='flag' />
 		</div>
 	);
 };
