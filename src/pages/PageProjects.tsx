3242732f75e56c6eadbccd104042997483cdd1df
InnersecApp <andrewpmarks@outlook.com>
2020-10-27 18:36:05 -0600

    Updated projects page

--- src/pages/PageProjects.tsx
+++ src/pages/PageProjects.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Project } from '../components/Project';
 
 export const PageProjects = () => {
+	const projects = [
+		{
+			name: 'Project Name',
+			link: '/projects',
+		},
+	];
 	return (
-		<div>
-			<div>Projects Page</div>
-			<div>Under Construction</div>
-		</div>
+		<section className="projects">
+			{projects.map((project, index) => (
+				<Project key={index} />
+			))}
+		</section>
 	);
 };
