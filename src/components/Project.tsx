b7d931a575b3aa028243ffb08868f2098b5007c8
InnersecApp <andrewpmarks@outlook.com>
2020-10-28 16:00:09 -0600

    Added project card image and fixed footer flexing issue

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,11 +8,18 @@ type ProjectProps = {
 };
 
 export const Project = (props: ProjectProps) => {
-	let { name, link } = props.project;
+	let { name, link, description, image } = props.project;
 	return (
 		<Link to={link} className='no-style link project'>
-			<aside className='hero'></aside>
-			<article className='info'>{name}</article>
+			{image && (
+				<aside className='hero'>
+					<img src={image} alt='' />
+				</aside>
+			)}
+			<article className='info'>
+				<div className='name'>{name}</div>
+				<div className='description'>{description}</div>
+			</article>
 		</Link>
 	);
 };
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -9,7 +9,14 @@ export const projects: ProjectType[] = [
 	{
 		name: 'AndrewMarks.io',
 		link: '/',
+		image: '/images/placeholder.png',
 		description:
 			'Lorem ipsum dolor sit amet, duis excepteur enim irure nisi nostrud velit occaecat tempor duis culpa exercitation sint elit minim consectetur culpa officia et pariatur',
+	},
+	{
+		name: 'Sudoku',
+		link: '/sudoku',
+		description:
+			'Reprehenderit laboris tempor do ex eu in ad labore incididunt Lorem cillum incididunt incididunt exercitation consectetur ad nisi exercitation deserunt id magna excepteur proident ullamco',
 	},
 ];
