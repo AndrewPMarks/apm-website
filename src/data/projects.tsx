ab3551ec93f6ef385dcb75eedd02a77ba07a4367
InnersecApp <andrewpmarks@outlook.com>
2020-11-12 13:58:48 -0700

    Changed project description styling

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -10,6 +10,9 @@ export const projects: ProjectType[] = [
 		name: 'AndrewMarks.io',
 		link: 'https://andrewmarks.io/',
 		image: '/images/projects/andrewmarks.io.png',
-		description: `This is my personal website that you are currently on. It is a single page application build using <a href="https://reactjs.org/">React</a>, <a href="https://sass-lang.com/">Sass</a>, and <a href="https://www.typescriptlang.org/">Typescript</a>. View this project on <a href="https://github.com/AndrewPMarks/apm-website">GitHub</a>`,
+		description: `This is my personal website that you are currently on. 
+			It is a single page application build using <a href="https://reactjs.org/">React</a>,
+			<a href="https://sass-lang.com/">Sass</a>, and <a href="https://www.typescriptlang.org/">Typescript</a>.
+			View this project on <a href="https://github.com/AndrewPMarks/apm-website">GitHub</a>`,
 	},
 ];
